0ad24828b3f2bb4489d38b6c9525bd6ad6527cda
Chris Liaw <chrisliaw@Dragonfly.local>
2021-08-13 12:33:53 +0800

    Fix library loading still on linux path

--- lib/oqs/common_wrapper.rb
+++ lib/oqs/common_wrapper.rb
@@ -5,8 +5,10 @@ require 'fiddle/import'
 module Oqs
   module CommonWrapper
    extend Fiddle::Importer
+   include Oqs::Wrapper
 
-   dlload File.join(File.dirname(__FILE__),"..","..","native","linux","x86_64","liboqs.so.0.7.0")
+   #dlload File.join(File.dirname(__FILE__),"..","..","native","linux","x86_64","liboqs.so.0.7.0")
+   load_oqs_lib
 
    extern 'int OQS_MEM_secure_bcmp(const void *a, const void *b, size_t len)'
    extern 'int OQS_MEM_cleanse(const void *ptr, size_t len)'
